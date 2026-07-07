426952cacc69943d38d4f584fc6232e78148ecbe
Polycarp Atalor <polycarpatalor@gmail.com>
2026-07-07 19:02:19 +0100

    Implemented login and set protected route for the dashboard

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,107 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { loginUser } from "../services/authService";
+
 function Login() {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+
+    };
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        setError("");
+
+        try {
+
+            const data = await loginUser(formData);
+
+            // Save JWT
+            localStorage.setItem("token", data.token);
+
+            // Save user
+            localStorage.setItem(
+                "user",
+                JSON.stringify(data.user)
+            );
+
+            navigate("/dashboard");
+
+        } catch (err) {
+
+            setError(
+                err.response?.data?.message ||
+                "Login failed."
+            );
+
+        }
+
+    };
+
     return (
-        <h1>Login Page</h1>
+
+        <div className="container">
+
+            <h1>Login</h1>
+
+            {error && <p className="error">{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+
+                <button type="submit">
+                    Login
+                </button>
+
+            </form>
+
+            <p>
+
+                Don't have an account?
+
+                <Link to="/register">
+
+                    Register
+
+                </Link>
+
+            </p>
+
+        </div>
+
     );
+
 }
 
 export default Login;
