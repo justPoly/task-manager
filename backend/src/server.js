1f389ce5ae3170a0fe7a77daccf361e46f2520cc
Polycarp Atalor <polycarpatalor@gmail.com>
2026-07-07 12:37:01 +0100

    Configure MongoDB Atlas connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
+connectDB();
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
